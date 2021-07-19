77143b44b5483923e7671985a55ea98169ba0d79
thytran38 <ax4409h@gmail.com>
2021-07-19 17:43:07 +0700

    Add detail job

---
+++ app/src/main/java/com/example/ejob/ui/user/application/ViewJobDetail.java
@@ -0,0 +1,16 @@
+package com.example.ejob.ui.user.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ejob.R;
+
+public class ViewJobDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_job_detail);
+    }
+}

1580804fd7e981cac0e59210de831db163b04fe2
thytran38 <ax4409h@gmail.com>
2021-07-20 15:58:30 +0700

    add submit successfully

--- app/src/main/java/com/example/ejob/ui/user/JobApplying.java
+++ app/src/main/java/com/example/ejob/ui/user/JobApplying.java
@@ -1,12 +1,16 @@
 package com.example.ejob.ui.user;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,11 +20,17 @@ import com.example.ejob.data.model.ApplicationStatus;
 import com.example.ejob.ui.user.application.JobApplication;
 import com.example.ejob.ui.user.userjob.JobPostingforUser;
 import com.example.ejob.utils.Date;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JobApplying extends AppCompatActivity {
 
@@ -32,8 +42,9 @@ public class JobApplying extends AppCompatActivity {
     private EditText getEtFullname, getEtPhone, getEtAddress, getEtEmail, getEtSchool, cletter;
     private RelativeLayout submit;
     private ImageView imgCv, imgLt;
+    JobPostingforUser jobPosting;
 
-    FirebaseFirestore firebaseFirestore;
+    FirebaseFirestore db;
     FirebaseDatabase firebaseDatabase;
     FirebaseAuth firebaseAuth;
     DocumentReference documentReference;
@@ -47,11 +58,11 @@ public class JobApplying extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
-        firebaseFirestore = FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         mapping();
 
-        JobPostingforUser jobPosting = getIntent().getExtras().getParcelable("myJobposting");
+        jobPosting = getIntent().getExtras().getParcelable("myJobposting");
 
         if(firebaseAuth.getCurrentUser().getDisplayName() == null){
             getEtFullname.setText("Enter Full Name");
@@ -100,6 +111,98 @@ public class JobApplying extends AppCompatActivity {
         }
 
 
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(allFieldValidation()){
+                    submitEvent(jobApplication, applicantModel);
+                    Toast.makeText(JobApplying.this, "Submit Application successfully!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+
+    private boolean allFieldValidation() {
+        boolean validate = false;
+        if(!isEmailValid(getEtEmail.getText().toString())){
+            Toast.makeText(this, "Enter valid email", Toast.LENGTH_SHORT).show();
+        }
+        validate = !(getEtFullname.getText().toString().equals("") && getEtPhone.getText().toString().equals("")
+                && isEmailValid(getEtEmail.getText().toString()) && getEtAddress.getText().toString().equals("")
+                && getEtSchool.getText().toString().equals(""));
+
+        return validate;
+    }
+
+    private  submitEvent(JobApplication application, ApplicantModel applicant) {
+
+        db.collection("Applications")
+                .document(jobPosting.getJobId())
+                .collection("pending")
+                .document(firebaseAuth.getCurrentUser().getUid())
+                .set(application)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(JobApplying.this, "Applied successfully for this " + jobPosting.getJobTitle() + " job.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(JobApplying.this, "Failed to apply. Please retry.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        db.collection("Applicants")
+                .document(firebaseAuth.getCurrentUser().getUid())
+                .set(applicant)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d("Applicants", "Succeeded add job to Applicants");
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Applicants", "Failed add job to Applicants");
+
+                    }
+                });
+
+        db.collection("JobAppliedHistory")
+                .document(firebaseAuth.getCurrentUser().getUid())
+                .collection("jobs")
+                .document(jobPosting.getJobId())
+                .set(application)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d("AppliedHistory", "Success add job to JobAppliedHistory");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("AppliedHistory", "Failure add job to JobAppliedHistory");
+
+                    }
+                });
 
     }
 
